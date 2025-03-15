95b33370e6130b281f7e45ded15e5bb3bfc045bd
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2025-03-15 00:31:01 +0000

    updated models

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = "user"
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
+    
+    reservations = db.relationship('Reservation', backref='user', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "is_active": self.is_active
+        }
+
+class Restaurant(db.Model):
+    __tablename__ = "restaurant"
+
+    restaurant_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "restaurant_id": self.restaurant_id,
+            "user_id": self.user_id
+        }
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+
+    favorite_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "favorite_id": self.favorite_id,
+            "user_id": self.user_id,
+            "restaurant_id": self.restaurant_id
+        }
+
+class Reservation(db.Model):
+    __tablename__ = "reservation"
+
+    reservation_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    time_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    reservation_time = db.Column(db.DateTime, nullable=False)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "reservation_id": self.reservation_id,
+            "user_id": self.user_id,
+            "restaurant_id": self.restaurant_id,
+            "time_created": self.time_created,
+            "reservation_time": self.reservation_time,
+            "is_active": self.is_active
         }
